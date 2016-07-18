4874e926fb140e6db82cacba03cf1cee0387532c
黎东潮 <lidc@pv25.com>
2016-07-18 15:23:48 +0800

    utf-8

--- application/views/layouts/index.php
+++ application/views/layouts/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="gb2312">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>米饭加盐（测试版）</title>
     <meta name="keywords" content="米饭加盐（测试版）,博客模板" />
     <meta name="description" content="测试版主题的个人博客模板，优雅、稳重、大气,低调。" />
@@ -14,7 +14,33 @@
 <body>
 <header>
     <div id="logo"><a href="/"></a></div>
-    <nav class="topnav" id="topnav"><a href="index.html"><span>首页</span><span class="en">Protal</span></a><a href="about.html"><span>关于我</span><span class="en">About</span></a><a href="newlist.html"><span>慢生活</span><span class="en">Life</span></a><a href="moodlist.html"><span>碎言碎语</span><span class="en">Doing</span></a><a href="share.html"><span>模板分享</span><span class="en">Share</span></a><a href="knowledge.html"><span>学无止境</span><span class="en">Learn</span></a><a href="book.html"><span>留言版</span><span class="en">Gustbook</span></a></nav>
+    <nav class="topnav" id="topnav">
+            <a href="index.html">
+            <span>首页</span>
+            <span class="en">Protal</span></a>
+        <a href="about.html">
+            <span>关于我</span>
+            <span class="en">About</span>
+        </a>
+        <a href="newlist.html">
+            <span>慢生活</span>
+            <span class="en">Life</span>
+        </a>
+        <a href="moodlist.html">
+            <span>碎言碎语</span>
+            <span class="en">Doing</span>
+        </a>
+        <a href="share.html">
+            <span>模板分享</span>
+            <span class="en">Share</span>
+        </a>
+        <a href="knowledge.html">
+            <span>学无止境</span>
+            <span class="en">Learn</span>
+        </a><a href="book.html">
+            <span>留言版</span>
+            <span class="en">Gustbook</span>
+        </a>
     </nav>
 </header>
 <?php echo $_content_ ?>
